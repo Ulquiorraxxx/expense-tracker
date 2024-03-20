0f333f3f16dcbcad10ae196d6c464910f4bd5995
Ronalds Polakovs <roncispolak07@gmail.com>
2024-03-20 09:27:09 +0200

    mamamia

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #  | |___ >  <| |_) |  __/ | | \__ \  __/\__ \   | || | | (_| | (__|   <  __/ |   
 #  |_____/_/\_\ .__/ \___|_| |_|___/\___||___/   |_||_|  \__,_|\___|_|\_\___|_|   
 #             |_|                                                                 
-
+import json
 #
 # Uzdevums:
 # Uzrakstīt programmu, kas ļauj
@@ -16,21 +16,63 @@
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
 
-expenses = []
 
+def sorting(expense):
+    return int(expense[1])
+
+
+expenses = []
+print(''''
+   _____                                       _____               _             
+  | ____|_  ___ __   ___ _ __  ___  ___  ___  |_   _| __ __ _  ___| | _____ _ __ 
+  |  _| \ \/ / '_ \ / _ \ '_ \/ __|/ _ \/ __|   | || '__/ _` |/ __| |/ / _ \ '__|
+  | |___ >  <| |_) |  __/ | | \__ \  __/\__ \   | || | | (_| | (__|   <  __/ |   
+  |_____/_/\_\ .__/ \___|_| |_|___/\___||___/   |_||_|  \__,_|\___|_|\_\___|_|   
+             |_|                                                                      
+      ''')
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
-pass
+
+expense_file = open('expenses.json') 
+expenses = json.load(expense_file) 
+expense_file.close()
+
 
 while True:
     command = input("\nChoose command:")
     if command == "1":
-        pass
+        nosaukums=input("Nosaukums: ")
+        summa=int(input("summa: "))
+        kategorija=input("Kategorija: ")
+        expenses.append([nosaukums , summa , kategorija])
+        
+    
+    if command == "2":
+        for expense in expenses:
+            print(expense)
+
+    if command == "+":
+        expenses.sort(key = sorting , reverse = True )
+        print(expenses[0:10])
+    if command == "-":
+        expenses.sort(key = sorting ,)
+        print(expenses[0:10])
+    if command =="v":
+        total = 0
+        total_nr=0
+        for expense in expenses:
+            total = total + expense[1]
+            total_nr=total_nr+1
+        print("Vidējā summa: ",total/total_nr)
+
     if command == "e":
         print("Exiting...")
         break
 
+with open("expenses.json", "w") as outfile:
+    json.dump(expenses, outfile)
+
 # save expenses to expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Writing JSON to a file in Python)
-pass
+
 
